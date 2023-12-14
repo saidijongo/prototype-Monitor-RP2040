398e15ad50cf7e3023888e7c06869189de91c89b
Jongo Said (김정고) <31678025+saidijongo@users.noreply.github.com>
2023-12-14 20:06:22 +0900

    Update moreprints2.ino

--- moreprints2.ino
+++ moreprints2.ino
@@ -35,7 +35,7 @@ void setup() {
 
   for (int i = 0; i < sizeof(RELAY_PIN) / sizeof(RELAY_PIN[0]); i++) {
     pinMode(RELAY_PIN[i], OUTPUT);
-    digitalWrite(RELAY_PIN[i], LOW);
+    digitalWrite(RELAY_PIN[i],HIGH);
   }
 }
 
